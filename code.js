3fac066dbb2d489ccfe0f01ebe2f5bb34481f81c
Avirup Chakraborty <70001662+avirup247@users.noreply.github.com>
2022-06-03 19:23:48 +0530

    Add files via upload

--- code.js
+++ code.js
@@ -1,12 +1,40 @@
+class obstacle{
+    constructor(position,velocity,id){
+        this.position=position;
+        this.element=document.getElementById(id);
+        this.id=id;
+        this.velocity=velocity;
+    }
+        obstacle(){
+            return this.element;
+        }
+        getPosition(){
+            return this.position;
+    }
+        setPosition(newPosition){
+            this.position=newPosition;
+        }
+        getVelocity(){
+            return this.velocity;
+        }
+        setVelocity(){
+            this.velocity=-Math.random()*2;
+        }
+
+}
+
+// obstacle variables
+let cactusArray =new Array(); //new obstacle(1000,-Math.random()-0.5,"cactus1");
+for(j=1;j<4;j++){
+    cactusArray[j]=new obstacle(1000*j,-Math.random()*2,"cactus"+j);
+    console.log(cactusArray[j]);
+}
 // variable for the ball or player
 var ball=document.getElementById("ball");
 var y=400;
 var v=-1;
-var g=-0.0098;
-//variables for the cactus
-var cactus= document.getElementById("cactus");
-var cac_x = 1000;
-var cac_v= -1;
+var g=-0.011;
+
 // variable for distance
 var distance;
 // now we are going to make animation with a loop
@@ -17,17 +45,19 @@ document.body.onkeydown=function(e){
     if(e.key==" " || e.code=="Space" || e.KeyCode==32){
         game();
         document.getElementById("over").style.visibility="hidden";
-        document.getElementById("over").innerHTML="GAME OVER";
-        //audio.play();
+        document.getElementById("over").innerHTML="GAME OVER<button onclick='location.reload()'>RELOAD</button>";
        
     }
 }
 var loop;
 function game(){
-    loop=setInterval(function(){
+loop=setInterval(function(){
     y=y+v;
     v=v+g;
-    cac_x=cac_x+cac_v;
+    cactusArray.forEach(cactus => {
+        cactus.setPosition(cactus.getPosition()+cactus.getVelocity());
+    });
+    
     // create bounce effect
     if(y<0){
         v=0;
@@ -42,15 +72,21 @@ function game(){
         }
     }
     // for collision detection
-    if(cac_x<200 && cac_x>100){
-        if(y<60){
-            clearInterval(loop);
-            document.getElementById("over").style.visibility="visible";    
-        }
-       }
+    cactusArray.forEach(cactus => {
+        if(cactus.getPosition()<200 && cactus.getPosition()>100){
+            if(y<60){
+                clearInterval(loop);
+                document.getElementById("over").style.visibility="visible";    
+            }
+           }
+    });
+    
     obstaclesLoop();
     ball.style.bottom= y+"px";
-    cactus.style.left=cac_x+"px";
+    cactusArray.forEach(cactus => {
+        document.getElementById(cactus.id).style.left=cactus.getPosition()+"px";
+
+    });
     },1);
     //for score 
     var scoreElement=document.getElementById("score");
@@ -61,18 +97,15 @@ function game(){
     }
     // loop for obstacles
     function obstaclesLoop(){
-        if(cac_x < 0){
-            cac_x=document.querySelector("body").offsetWidth+ Math.random()*500;
-            increaseScore();
-        }
+        cactusArray.forEach(cactus => {
+            if(cactus.getPosition() < 0){
+                cactus.setPosition(document.querySelector("body").offsetWidth+ Math.random()*500);
+                cactus.setVelocity();
+                increaseScore();
+            }
+        });
+       
     }
 
 }
-function cl(){
-if(loop==null){
-game();
-}
-audio.play();
-      y=1;
-      v=2;
-}
+
